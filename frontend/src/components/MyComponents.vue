45e4762c93d703685271b539f3a56ba59219170b
Islam Darbishev <islamdarbishev01@gmail.con>
2022-06-07 03:43:25 +0300

    master

---
+++ frontend/src/components/MyComponents.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="root">
+    <div >
+      <div class="img" >
+        <img
+          v-show="isCatVisible"
+          width="500px"
+          src="..\public\vkPhoto.jpg"
+          alt="my photo"
+        />
+      </div>
+      <div class="img">
+        <button @click="isCatVisible = !isCatVisible">
+           Показать/спрятать {{ isCatVisible }}
+        </button>
+      </div>
+    </div>
+    <div>
+      <hr />
+    <label
+      >Введите email
+      <input
+        type="email"
+        :value="email"
+        @input="emailInput($event.target.value)"
+      />
+    </label>
+    <label
+      >Введите пароль
+      <input
+        type="password"
+        :value="password"
+        @input="passwordInput($event.target.value)"
+      />
+    </label>
+    {{ userName }}
+    <p>Количество участников : {{ users.length }}</p>
+    <ul>
+      <li v-for="(user, index) of users" :key="index">
+        {{ `${index + 1} ${getFullName(user)}` }}
+      </li>
+    </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Mycomponents",
+  data() {
+    return {
+      isCatVisible: "true",
+      email: "example@mail.ru",
+      password: "123656",
+      users: [
+        {
+          firstName: "Islam",
+          secondName: "Darbishev",
+        },
+        {
+          firstName: "Tasya",
+          secondName: "Sidorkina",
+        },
+        {
+          firstName: "Ismail",
+          secondName: "Hanaliev",
+        },
+      ],
+      pages: 3,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    userName() {
+      return `${this.email}  ${this.password}`.toUpperCase();
+    },
+  },
+  methods: {
+    getFullName(user) {
+      return user.firstName + " " + user.secondName;
+    },
+    emailInput(value) {
+      this.email = value;
+    },
+    passwordInput(value) {
+      this.password = value;
+    },
+  },
+};
+</script>
+<style scoped>
+button {
+  margin: 20px;
+}
+.img{
+  display: inline-block;
+}
+</style>
---
+++ frontend/src/views/ProductList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="product-list">
+    <h1>Письменные столы</h1>
+    <input type="text" class="search" v-model="searchText">
+    {{searchText}}
+    <ul class="list-defaultflex">
+      <li v-for="(product, index) in products" :key="index">
+        <product-card
+          :title="product.title"
+          :price="product.price"
+          :img-url="product.imgUrl"
+          :count="product.count"
+          @addToBasket="addToBasket(index)"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ProductCard from "./ProguctCard.vue";
+
+export default {
+  name: "ProductList",
+  components: { ProductCard },
+  data() {
+    return {
+        searchText:'',
+      products: [
+        {
+          title: "Стол бомба",
+          price: 199464299,
+          imgUrl:
+            "https://main-cdn.sbermegamarket.ru/hlr-system/1663694819/100024922066b0.jpg",
+          count: 40,
+        },
+        {
+          title: "Стол пушка",
+          price: 157984599,
+          imgUrl:
+            "https://main-cdn.sbermegamarket.ru/hlr-system/1663694819/100024922066b0.jpg",
+          count: 50,
+        },
+        {
+          title: "Стол дерево",
+          price: 1995749,
+          imgUrl:
+            "https://main-cdn.sbermegamarket.ru/hlr-system/1663694819/100024922066b0.jpg",
+          count: 10,
+        },
+      ],
+    };
+  },
+  methods: {
+        addToBasket(index){
+            console.log(index)
+        }
+    },
+};
+</script>
+
+<style scoped>
+ul li {
+  font-size: 19px;
+  display: inline-block;
+  width: 30%;
+  margin-left: 3%;
+}
+</style>
